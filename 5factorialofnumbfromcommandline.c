663a837548b9dee98097bfbfdf57957b9198726a
NeerajKondalkar <neerajkondalkar@yahoo.in>
2020-10-20 21:25:37 +0530

    factorial now with command line

--- 5factorialofnumbfromcommandline.c
+++ 5factorialofnumbfromcommandline.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 long long  int fact(int n){
 	int i;
@@ -14,10 +16,25 @@ long long  int fact(int n){
 	return fac;
 }
 
-void main(){
-	int n;
-	printf("Enter number : ");
-	scanf("%d", &n);
+int main(int argc, char *argv[]){
+	int n, i;
+	if(argc < 2 || argc > 2){
+		printf("Usage : <program_name>  <number>\n");
+		exit(0);
+	}
 	printf("\n");
+	printf("strlen of argv[1] = %d\n", strlen(argv[1]));
+	//n = *argv[1];
+	//printf(" n = %d    [%xh]\n", n, n);
+	printf("printing each character of argv[1] \n");
+	for(i=0; i<strlen(argv[1]); i++){
+		printf(" argv[1][%d]  =   %d   [%xh]\n", i, argv[1][i], argv[1][i]);
+		if(argv[1][i] > 57 || argv[1][i] < 48){
+			printf("please enter only integers \n");
+			exit(0);
+		}
+	}
+	n = atoi(argv[1]);
+	printf(" n = %d    [%xh]\n", n, n);
 	printf("factorial of %d is %lld\n", n, fact(n));
 }
